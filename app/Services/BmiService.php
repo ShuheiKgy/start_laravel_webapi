5868b3afac66c98a179e62006d67ad1c5ba659a0
Shuhei Kagaya <shuhei.lab@gmail.com>
2018-07-18 09:08:26 +0900

    BmiServiceクラス作成とRouteの設定
    * BmiServiceクラス作成
    * Routeの設定

---
+++ app/Services/BmiService.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shuhei-k
+ * Date: 2018/07/17
+ * Time: 22:31
+ */
+
+namespace App\Services;
+
+
+use App\Models\Person;
+
+class BmiService
+{
+    public static function getBmi(Person $person)
+    {
+
+    }
+
+    private static function calcBmi(float $height, float $weight)
+    {
+
+    }
+
+}
---
+++ tests/Unit/Services/BmiServiceTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Unit\Services;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class BmiServiceTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testCalcBmi($height, $weight, $result)
+    {
+
+    }
+
+    public function testGetBmi($height, $weight, $result)
+    {
+
+    }
+
+    public function bmiDataProvider()
+    {
+
+    }
+}
