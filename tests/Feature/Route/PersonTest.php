ac9638b9989d897d24372da0bff71d8f44fc82c6
Shuhei Kagaya <shuhei.lab@gmail.com>
2018-07-19 22:51:01 +0900

    Mockery導入
    * Mockery導入

--- tests/Feature/Route/PersonTest.php
+++ tests/Feature/Route/PersonTest.php
@@ -2,12 +2,47 @@
 
 namespace Tests\Feature\Route;
 
+use Mockery;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class PersonTest extends TestCase
 {
+
+    protected $personMock;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->personMock = Mockery::mock('App\Models\Person');
+        $this->personMock
+            ->shouldReceive('getAttribute')
+            ->with('id')
+            ->andReturn(1);
+        $this->personMock
+            ->shouldReceive('getAttribute')
+            ->with('name')
+            ->andReturn('Jessie');
+        $this->personMock
+            ->shouldReceive('getAttribute')
+            ->with('height')
+            ->andReturn(1.73);
+        $this->personMock
+            ->shouldReceive('getAttribute')
+            ->with('weight')
+            ->andReturn(59.0);
+        $this->personMock
+            ->shouldReceive('getAttribute')
+            ->andReturn(null);
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+        Mockery::close();
+    }
+
     public function testPersonList()
     {
         $response = $this->json('GET', 'api/person');
