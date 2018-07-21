14792fa08064b03ab795db46f089983426c1f284
Shuhei Kagaya <shuhei.lab@gmail.com>
2018-07-21 23:32:12 +0900

    PersonControllerの実装
    * PersonControllerの実装

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\PersonCollection;
+use App\Http\Resources\PersonResource;
 use App\Models\Person;
 use Illuminate\Http\Request;
 
@@ -10,11 +12,11 @@ class PersonController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return PersonCollection
      */
-    public function index()
+    public function index(Person $person)
     {
-        //
+        return new PersonCollection($person);
     }
 
     /**
@@ -31,22 +33,29 @@ class PersonController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return PersonResource
      */
-    public function store(Request $request)
+    public function store(Person $person, Request $request)
     {
-        //
+        $p = $person->create(
+            [
+                'name' => $request->input('name'),
+                'height' => $request->input('height'),
+                'weight' => $request->input('weightn'),
+            ]
+        );
+        return new PersonResource($p);
     }
 
     /**
      * Display the specified resource.
      *
      * @param  \App\Models\Person  $person
-     * @return \Illuminate\Http\Response
+     * @return PersonResource
      */
     public function show(Person $person)
     {
-        //
+        return new PersonResource($person);
     }
 
     /**
@@ -65,21 +74,41 @@ class PersonController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \App\Models\Person  $person
-     * @return \Illuminate\Http\Response
+     * @return PersonResource
      */
     public function update(Request $request, Person $person)
     {
-        //
+        $isUpdated = false;
+        if ($request->input('name')) {
+            $person->name = $request->input('name');
+            $isUpdated = true;
+        }
+        if ($request->input('height')) {
+            $person->height = $request->input('height');
+            $isUpdated = true;
+        }
+        if ($request->input('weight')) {
+            $person->weight = $request->input('weight');
+            $isUpdated = true;
+        }
+
+        if ($isUpdated) {
+            $person->save();
+        }
+
+        return new PersonResource($person);
+
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  \App\Models\Person  $person
-     * @return \Illuminate\Http\Response
+     * @return PersonResource
      */
     public function destroy(Person $person)
     {
-        //
+        $person->delete();
+        return new PersonResource($person);
     }
 }
